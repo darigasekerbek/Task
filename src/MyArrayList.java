b0f229876448860a0ecfb637da72978f9d33abe5
Abylai <221714@astanait.edu.kz>
2023-04-19 16:38:38 +0200

    clear() for MyArrayList Object get(int index)

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -80,7 +80,8 @@ public class MyArrayList <T> implements MyList{
 
     @Override
     public Object get(int index) {
-        return null;
+        checkIndex(index);
+        return arr[index];
     }
 
     @Override
